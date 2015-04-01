404cc9bf525c0ab17d976abe30517ec57216be4b
Hastu <abdellihaithemensi@gmail.com>
2015-04-01 12:54:06 +0100

    urls.py

--- educ/educ/urls.py
+++ educ/educ/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from forum.views import *
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', Forum.as_view(), name='forum'),
+    url(r'^api-auth/post/$', ForumPostList.as_view(), name='forumpost-list'),
+    url(r'^api-auth/post/(?P<pk>\d+)/$', ForumPostDetail.as_view(), name='forumpost-detail'),
+    url(r'^api-auth/comment/$', ForumCommentList.as_view(), name='forumcomment-list'),
+    url(r'^api-auth/comment/(?P<ck>\d+)/$', ForumCommentDetail.as_view(), name='forumcomment-detail'),
 )
